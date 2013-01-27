bf8f1c18ee5bcc805bad9271c6ee53e014f1692f
Harish Madabushi <harish.tmh@gmail.com>
2013-01-27 14:17:40 +0530

    XML/Smart/Entity.pm uses warnings

--- lib/XML/Smart/Entity.pm
+++ lib/XML/Smart/Entity.pm
@@ -2,7 +2,7 @@
 ## Name:        Entity.pm
 ## Purpose:     XML::Smart::Entity - Handle entities
 ## Author:      Graciliano M. P.
-## Modified by:
+## Modified by: Harish Madabushi
 ## Created:     28/09/2003
 ## RCS-ID:      
 ## Copyright:   (c) 2003 Graciliano M. P.
@@ -10,35 +10,44 @@
 ##              modify it under the same terms as Perl itself
 #############################################################################
 
-package XML::Smart::Entity ;
+package XML::Smart::Entity                                     ;
+
+use strict                                                     ;
+use warnings                                                   ;
+
+require Exporter                                               ;
+
+use XML::Smart::Shared qw( _unset_sig_warn _reset_sig_warn )   ;
 
 our ($VERSION , @ISA) ;
-$VERSION = '0.01' ;
+$VERSION = '0.03'     ;
 
-require Exporter ;
-@ISA = qw(Exporter) ;
+@ISA = qw(Exporter)        ;
 
 our @EXPORT = qw(_parse_basic_entity _add_basic_entity) ;
 our @EXPORT_OK = @EXPORT ;
 
-use strict ;
-no warnings ;
+
 
 #######################
 # _PARSE_BASIC_ENTITY #
 #######################
 
 sub _parse_basic_entity {
-  $_[0] =~ s/&lt;/</gs ;
-  $_[0] =~ s/&gt;/>/gs ;
-  $_[0] =~ s/&amp;/&/gs ;
-  $_[0] =~ s/&apos;/'/gs ;
-  $_[0] =~ s/&quot;/"/gs ;
+
+    my $entity = $_[0] ;
+    if( $entity ) { 
+	$_[0] =~ s/&lt;/</gs ;
+	$_[0] =~ s/&gt;/>/gs ;
+	$_[0] =~ s/&amp;/&/gs ;
+	$_[0] =~ s/&apos;/'/gs ;
+	$_[0] =~ s/&quot;/"/gs ;
+	
+	$_[0] =~ s/&#(\d+);/ $1 > 255 ? pack("U",$1) : pack("C",$1)/egs ;
+	$_[0] =~ s/&#x([a-fA-F\d]+);/pack("U",hex($1))/egs ;
+    }
   
-  $_[0] =~ s/&#(\d+);/ $1 > 255 ? pack("U",$1) : pack("C",$1)/egs ;
-  $_[0] =~ s/&#x([a-fA-F\d]+);/pack("U",hex($1))/egs ;
-  
-  return( $_[0] ) ;
+  return( $entity ) ;
 }
 
 #####################
@@ -46,19 +55,32 @@ sub _parse_basic_entity {
 #####################
 
 sub _add_basic_entity {
-  $_[0] =~ s/(&(?:\w+;)?)/{_is_amp($1) or $1}/sgex ;
-  $_[0] =~ s/</&lt;/gs ;
-  $_[0] =~ s/>/&gt;/gs ;
-  return( $_[0] ) ;
-}
 
+    my $entity = $_[0] ;
+    if( $entity ) { 
+	$_[0] =~ s/(&(?:\w+;)?)/{_is_amp($1) or $1}/sgex ;
+	$_[0] =~ s/</&lt;/gs ;
+	$_[0] =~ s/>/&gt;/gs ;
+    }
+
+    return( $entity ) ;
+}
+    
 ###########
 # _IS_AMP #
 ###########
 
 sub _is_amp {
-  if ($_[0] eq '&') { return( '&amp;' ) ;}
-  return( undef ) ;
+
+    my $entity = $_[0] ;
+    if( $entity ) { 
+	if($entity eq '&') { 
+	    return( '&amp;' ) ;
+	}
+    }
+
+    return( undef ) ;
+
 }
 
 #######
